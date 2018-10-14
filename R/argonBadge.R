8be88a6a5f8c41f8a108e69cf223b5dbfca0cd7f
Divad0175 <dgranjon@ymail.com>
2018-10-14 14:42:32 +0200

    add badges

---
+++ R/argonBadge.R
@@ -0,0 +1,30 @@
+#' Create a Boostrap 4 badge
+#'
+#' Build an argon badge
+#'
+#' @param text Badge text.
+#' @param src Badge external link.
+#' @param pill Pill mode. FALSE by default.
+#' @param status Badge status.
+#' 
+#' @examples 
+#' if (interactive()) {
+#' }
+#' 
+#' 
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+argonBadge <- function(text = NULL, src = NULL, pill = FALSE, status = "default") {
+  
+  badgeCl <- "badge"
+  if (pill) badgeCl <- paste0(badgeCl, " badge-pill")
+  if (status) badgeCl <- paste0(badgeCl, " badge-", status)
+  
+  if (!is.null(src)) {
+    htmltools::a(href = src, class = badgeCl, text)
+  } else {
+    htmltools::tags$span(class = badgeCl, text)
+  }
+  
+}
