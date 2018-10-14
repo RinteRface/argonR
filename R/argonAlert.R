46fede5687b22f9dd167888ac258afe073c4f6da
Divad0175 <dgranjon@ymail.com>
2018-10-14 14:54:30 +0200

    add alerts

---
+++ R/argonAlert.R
@@ -0,0 +1,47 @@
+#' Create a Boostrap 4 alert
+#'
+#' Build an argon alert
+#'
+#' @param ... Alert content.
+#' @param icon Alert icon.
+#' @param status Alert status.
+#' @param closable Whether to make the alert closable or not. TRUE by default.
+#' 
+#' @examples 
+#' if (interactive()) {
+#' }
+#' 
+#' 
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+argonAlert <- function(..., icon = NULL, status = "default", closable = TRUE) {
+  
+  alertCl <- "alert"
+  if (!is.null(status)) alertCl <- paste0(alertCl, " alert-", status)
+  
+  htmltools::tags$div(
+    class = alertCl,
+    role = "alert",
+    if (!is.null(icon)) {
+      htmltools::tags$span(
+        class = "alert-inner--icon",
+        htmltools::tags$i(class = "ni ni-", icon)
+      )
+    },
+    htmltools::tags$span(
+      class = "alert-inner--text",
+      ...
+    ),
+    if (closable) {
+      htmltools::tags$button(
+        class = "close",
+        type = "button",
+        `data-dismiss` = "alert",
+        `aria-label` = "close",
+        htmltools::tags$span(`aria-hidden` = "true", "&times;")
+      ) 
+    }
+  )
+  
+}
